46968ea85308c4be20a2d3b2072a339807ff8b9c
somya3819 <somyamahajan597@gmail.com>
2025-04-09 18:37:57 +0530

    initial commit 1

--- LL2/src/main/java/org/example/Main.java
+++ LL2/src/main/java/org/example/Main.java
@@ -30,7 +30,7 @@ class Operations{
         head=newNode;
     }
 
-    public void insertAtEnd(){
+    public void insertAtEnd(int data){
         Node newNode=new Node(data);
         if(head==null){
             head=newNode;
@@ -44,7 +44,7 @@ class Operations{
         newNode.prev=temp;
     }
 
-    public void InsertAtPos(int pos, int data){
+    public void insertAtPos(int pos, int data){
         if(pos<=1){
             insertAtBeginning(data);
             return;
@@ -54,7 +54,7 @@ class Operations{
             temp=temp.next;
         }
         if(temp==null || temp.next==null){
-            insertAtEnd();
+            insertAtEnd(data);
             return;
         }
         Node newNode=new Node(data);
@@ -85,22 +85,66 @@ class Operations{
         temp.prev.next=null;
     }
     public void DeleteFromPos(int pos){
-
-
+        if(head==null || pos<=1){
+            DeleteFromBegin();
+            return;
+        }
+        Node temp=head;
+        for(int i=1 ; i<pos && temp!=null ;i++){
+            temp=temp.next;
+        }
+        if(temp==null) return;
+        if(temp.next!=null){
+            temp.next.prev=temp.prev;
+        }
+        if(temp.prev!=null){
+            temp.prev.next=temp.next;
+        }
     }
 
     //print forward
     public void printForward(){
-
+        Node temp = head;
+        System.out.print("Forward: ");
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
     }
 
     //print backward
     public void PrintBackward(){
-
+        if (head == null) return;
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        System.out.print("Backward: ");
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.prev;
+        }
+        System.out.println();
     }
-}
+    }
+
 public class Main{
     public static void main(String[] args) {
+Operations dll=new Operations();
+dll.insertAtBeginning(5);
+dll.insertAtEnd(4);
+dll.insertAtPos(3,16);
+
+dll.printForward();
+dll.PrintBackward();
+
+dll.DeleteFromBegin();
+dll.DeleteFromEnd();
+dll.DeleteFromPos(2);
+
+dll.printForward();
+dll.PrintBackward();
 
     }
 }
