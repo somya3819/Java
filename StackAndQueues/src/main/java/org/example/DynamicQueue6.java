db21c245fea3de9f1fe68f9d52ca1d28a72e9e1e
somya3819 <somyamahajan597@gmail.com>
2025-04-24 17:16:15 +0530

    24 april 2025!

--- StackAndQueues/src/main/java/org/example/DynamicQueue6.java
+++ StackAndQueues/src/main/java/org/example/DynamicQueue6.java
@@ -1,19 +1,19 @@
-package org.example;
-
-public class DynamicQueue6 extends CircularQueue5{
-    public DynamicQueue6(int size){
-        super(size);
-    }
-    public DynamicQueue6(){
-        super();
-    }
-    @Override
-    public boolean insert(int item){
-        if(this.isFull()){
-            int[] temp=new int[data.length*2];
-        }
-
-        return super.insert(item);
-    }
-
-}
+//package org.example;
+//
+//public class DynamicQueue6 extends CircularQueue5{
+//    public DynamicQueue6(int size){
+//        super(size);
+//    }
+//    public DynamicQueue6(){
+//        super();
+//    }
+//    @Override
+//    public boolean insert(int item){
+//        if(this.isFull()){
+//            int[] temp=new int[data.length*2];
+//        }
+//
+//        return super.insert(item);
+//    }
+//
+//}
